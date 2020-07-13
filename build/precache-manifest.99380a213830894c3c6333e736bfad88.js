f0a5b5d432edb9f73ebb9a0d1b105b6f3cd6823f
Juho Lehtimaki <juho.a.lehtimaki@gmail.com>
2020-07-13 20:48:29 +0300

    uibuild

---
+++ build/precache-manifest.99380a213830894c3c6333e736bfad88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44681c3b1d8da432dde6cc02d0c48c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4d4deb86fd3ec31ff6",
+    "url": "/static/css/main.0c8b143e.chunk.css"
+  },
+  {
+    "revision": "161db1d91f5df0c67087",
+    "url": "/static/js/2.136d106e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.136d106e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4d4deb86fd3ec31ff6",
+    "url": "/static/js/main.21b0c5d8.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 //morgan conf
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 const app = express();
+app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
